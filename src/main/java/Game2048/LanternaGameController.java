3a11501669d0b0b664a8503dc2e387737f3f3c93
Szymon Kubica <szymo.kubica@gmail.com>
2022-04-27 11:19:38 +0200

    Added the LanternaGameController which allows for controlling the app using the lanterna library

---
+++ src/main/java/Game2048/LanternaGameController.java
@@ -0,0 +1,29 @@
+package Game2048;
+
+import com.googlecode.lanterna.input.KeyType;
+
+import java.io.IOException;
+
+public class LanternaGameController implements GameController {
+    private final LanternaBackendAdapter adapter;
+    public LanternaGameController(LanternaBackendAdapter adapter) {
+        this.adapter = adapter;
+    }
+    @Override
+    public Direction getMove() {
+        KeyType key;
+        try {
+            key = adapter.getGameHostTerminal().readInput().getKeyType();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return switch (key) {
+            case ArrowLeft -> Direction.LEFT;
+            case ArrowUp -> Direction.UP;
+            case ArrowDown -> Direction.DOWN;
+            case ArrowRight -> Direction.RIGHT;
+            default -> throw new IllegalStateException("Unexpected value: " + key);
+        };
+
+    }
+}
