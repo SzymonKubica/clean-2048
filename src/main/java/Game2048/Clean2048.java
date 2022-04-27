f5f6425cc09e72c57f9e481f2bec8df38d8a3d82
Szymon Kubica <szymo.kubica@gmail.com>
2022-04-27 11:21:09 +0200

    Update the main application to use introduced interfaces and not rely on the concrete library implementation. That way the coupling is greatly reduced

--- src/main/java/Game2048/Clean2048.java
+++ src/main/java/Game2048/Clean2048.java
@@ -1,30 +1,25 @@
 package Game2048;
 
-import com.googlecode.lanterna.input.KeyStroke;
-import com.googlecode.lanterna.input.KeyType;
 
 import java.util.Scanner;
 
 public class Clean2048 {
     public static void main(String[] args) {
-        Grid gameGrid = new Grid(4);
-        GameView view = new GameView(gameGrid);
+
+        LanternaBackendAdapter adapter = new LanternaBackendAdapter();
+        GameEngine engine = new GameEngine(4);
+        GameView view = new TerminalGameView(engine, adapter);
+        GameController controller = new LanternaGameController(adapter);
 
         Scanner sc = new Scanner(System.in);
-        view.startDisplay();
-        gameGrid.spawnTile();
-        while (!gameGrid.isGameOver()) {
+        engine.spawnTile();
+        while (!engine.isGameOver()) {
             view.updateDisplay();
-            Tile[][] oldGrid = gameGrid.grid.clone();
-            KeyType key = view.getInput().getKeyType();
-            switch (key) {
-                case ArrowLeft -> gameGrid.shift(Grid.Direction.LEFT);
-                case ArrowUp -> gameGrid.shift(Grid.Direction.UP);
-                case ArrowDown -> gameGrid.shift(Grid.Direction.DOWN);
-                case ArrowRight -> gameGrid.shift(Grid.Direction.RIGHT);
-            }
-            if (gameGrid.theGridChangedFrom(oldGrid)) {
-                gameGrid.spawnTile();
+            Tile[][] oldGrid = engine.grid.clone();
+            engine.shift(controller.getMove());
+
+            if (engine.theGridChangedFrom(oldGrid)) {
+                engine.spawnTile();
             }
         }
         view.updateDisplay();
